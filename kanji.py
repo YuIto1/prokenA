7219ef06486fb102fa2cf887751de4eebcff371b
Yuta <i.yuta.j20@gmail.com>
2025-06-24 08:26:25 +0000

    漢字の割合調査プログラム

---
+++ kanji.py
@@ -0,0 +1,17 @@
+import csv #csvファイル入力と過程
+import unicodedata #符号化文字集合をunicodeとする 便利ライブラリ
+
+total = 0 #全体の文字数を測定する
+ktotal = 0 #漢字の文字数を測定する
+with open("kanji.csv", mode = "r", encoding = 'utf-8') as f: #with open
+    reader = csv.reader(f) #各行に関するイテレータreader 
+    for row in reader: # 各行をリスト化して対応
+        for cell in row: #各リストを文字列化
+            for char in cell: # 文字列を一文字ずつ読む
+                total += 1
+                if('CJK UNIFIED IDEOGRAPH' in unicodedata.name(char, '')): #unicode.nameにて漢字ならCJK ~~ が出力される
+                    ktotal += 1
+    
+    print("全体文字数:", total,"漢字文字数", ktotal, "漢字割合", round(ktotal / total,4))
+
+
