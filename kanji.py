e745ea6d7f1b5f6bd61944f5eecc212efb5bc080
Yuta <i.yuta.j20@gmail.com>
2025-06-24 08:46:08 +0000

    関数化して汎用化しました

--- kanji.py
+++ kanji.py
@@ -1,6 +1,15 @@
 import csv #csvファイル入力と過程
 import unicodedata #符号化文字集合をunicodeとする 便利ライブラリ
 
+def is_kanji(char):
+    try:
+        return 'CJK UNIFIED IDEOGRAPH' in unicodedata.name(char, '')  #unicode.nameにて漢字ならCJK ~~ が出力される
+
+    except Exception:
+        print("Value Error", char) 
+        return False
+         
+    
 total = 0 #全体の文字数を測定する
 ktotal = 0 #漢字の文字数を測定する
 with open("kanji.csv", mode = "r", encoding = 'utf-8') as f: #with open
@@ -9,9 +18,6 @@ with open("kanji.csv", mode = "r", encoding = 'utf-8') as f: #with open
         for cell in row: #各リストを文字列化
             for char in cell: # 文字列を一文字ずつ読む
                 total += 1
-                if('CJK UNIFIED IDEOGRAPH' in unicodedata.name(char, '')): #unicode.nameにて漢字ならCJK ~~ が出力される
-                    ktotal += 1
+                ktotal = ktotal + int(is_kanji(char)) 
     
     print("全体文字数:", total,"漢字文字数", ktotal, "漢字割合", round(ktotal / total,4))
-
-
